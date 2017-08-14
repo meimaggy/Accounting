ba73cecc5b255af1996bdcbe589da5b918f75214
鄭美泓 <maggy.cheng@udngroup.com.tw>
2017-08-14 11:16:29 +0800

    新增EFUnitOfWork

---
+++ Accounting/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Accounting.Repositories
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public EFUnitOfWork()
+        {
+            Context = new AccountBookEntities();
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
