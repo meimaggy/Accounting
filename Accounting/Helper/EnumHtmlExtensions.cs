f9f371a6ad1285fa279228bce0eec105636ba80b
鄭美泓 <maggy.cheng@udngroup.com.tw>
2017-08-21 16:32:05 +0800

    加入Enum Category和EnumHtmlExtensions

---
+++ Accounting/Helper/EnumHtmlExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accounting.Helper
+{
+    public static class EnumHtmlExtensions
+    {
+        public static string GetDisplayName(this HtmlHelper helper, Enum enumType)
+        {
+            return enumType.GetType()
+                        .GetMember(enumType.ToString())
+                        .First()
+                        .GetCustomAttribute<DisplayAttribute>()
+                        .Name;
+        }
+    }
+}
---
+++ Accounting/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Accounting.Models
+{
+    public enum Category
+    {
+        [Display(Name = "支出")]
+        Expend,
+        [Display(Name = "收入")]
+        Income
+    }
+}
