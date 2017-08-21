0cd4458296ec19b915d851f5ae25413378146fca
鄭美泓 <maggy.cheng@udngroup.com.tw>
2017-08-21 16:34:44 +0800

    修正viewmodel Category型態別Enum Category

--- Accounting/Models/ViewModels/AccountingViewModel.cs
+++ Accounting/Models/ViewModels/AccountingViewModel.cs
@@ -7,7 +7,7 @@ namespace Accounting.Models.ViewModels
 {
     public class AccountingViewModel
     {        
-        public string Category { get; set; }
+        public Category Category { get; set; }
         public DateTime AccountingDate { get; set; }
         public int Amount { get; set; }
     }
