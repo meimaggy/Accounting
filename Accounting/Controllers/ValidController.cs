5b74157383cc545bd859f13d055ecc692286cb59
鄭美泓 <maggy.cheng@udngroup.com.tw>
2017-08-23 16:19:10 +0800

    刪除CategoryViewModel和新增ValidController (Remote驗證)

---
+++ Accounting/Controllers/ValidController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accounting.Views.Home
+{
+    public class ValidController : Controller
+    {
+        public ActionResult AmountValid(int amount)
+        {
+            bool isValidate = amount >= 1 && amount<= Int32.MaxValue;
+            return Json(isValidate, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AccountingDateValid(string accountingDate)
+        {
+            bool isValidate = Convert.ToDateTime(accountingDate) <= DateTime.Today;
+            return Json(isValidate, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
